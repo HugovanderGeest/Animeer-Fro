c2f4fb4feaad12769a63ee18031d9c65bac15f04
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-11-30 16:30:01 +0100

    6

--- animaties.js
+++ animaties.js
@@ -1,8 +1,20 @@
 // klin k op animeer-knop vlakje animeern 
 document.querySelector('.animeer-knop').addEventListener('click', (e) => {
     document.querySelector('.vlakje').classList.toggle('vlakje_naar-rechts')
-})
+});
 
+// block laat tijd
 setTimeout(()=> {
     document.querySelector('.vlakje').classList.toggle('vlakje_naar-rechts')
-}, 1000)
+}, 1000);
+
+// nav animatie
+const menuKnop = document.querySelector('.nav__knop');
+const navigatieItems = document.querySelectorAll('.nav__link');
+menuKnop.addEventListener('click', () => {
+    navigatieItems.forEach((item, index) => {
+        setTimeout( () => {
+            item.classList.toggle('nav__link--schuif-in');
+        } , 30 * index);
+    });
+});
