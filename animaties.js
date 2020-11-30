015797e89f434efb3cfe4cce8f267533381d3ac3
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-11-30 16:11:19 +0100

    5

--- animaties.js
+++ animaties.js
@@ -2,3 +2,7 @@
 document.querySelector('.animeer-knop').addEventListener('click', (e) => {
     document.querySelector('.vlakje').classList.toggle('vlakje_naar-rechts')
 })
+
+setTimeout(()=> {
+    document.querySelector('.vlakje').classList.toggle('vlakje_naar-rechts')
+}, 1000)
