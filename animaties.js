1d9e9ad48696a7d3c916353dfc1792b7125e0d47
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-11-30 13:25:02 +0100

    2

---
+++ animaties.js
@@ -0,0 +1,4 @@
+// klin k op animeer-knop vlakje animeern 
+document.querySelector('.animeer-knop').addEventListener('click', (e) => {
+    document.querySelector('.vlakje').classList.toggle('vlakje_naar-rechts')
+})
